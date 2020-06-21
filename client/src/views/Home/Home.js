76c90643fd47ae50106734ba4187ae6e73726fd5
Guy Brill <brill.guy1@gmail.com>
2020-06-21 16:02:10 +0300

    home view cleanup

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,26 +1,8 @@
-import React, { useEffect, useCallback } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 
 import GamesList from "../../components/GamesList/GamesList";
-import useDb from "../../hooks/useDb";
-
-import { getAllGames } from "../../actions";
 
 function Home() {
-  const dispatch = useDispatch();
-
-  const { status: dbStatus } = useDb();
-
-  const getAllPlayedGames = useCallback(() => dispatch(getAllGames()), [
-    dispatch,
-  ]);
-
-  useEffect(() => {
-    if (dbStatus === "success") {
-      getAllPlayedGames();
-    }
-  }, [dbStatus, getAllPlayedGames]);
-
   return (
     <>
       <GamesList />
