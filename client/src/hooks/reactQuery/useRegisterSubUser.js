2f24ab5cc84eaa620061d9fb5074421fa1e557f9
Guy Brill <brill.guy1@gmail.com>
2021-02-09 10:43:44 +0200

    register sub user fix

--- client/src/hooks/reactQuery/useRegisterSubUser.js
+++ client/src/hooks/reactQuery/useRegisterSubUser.js
@@ -6,36 +6,42 @@ import { registerUser } from '../../api';
 export default function useRegisterSubUser(cb) {
   const queryClient = useQueryClient();
   const [status, setStatus] = useState();
-  const registerSubUser = useMutation((user) => registerUser(user), {
-    onMutate: async (user) => {
-      setStatus('pending');
-      await queryClient.cancelQueries('users');
-
-      const previousValue = queryClient.getQueryData('users');
-
-      queryClient.setQueryData('users', (oldUsers) => {
-        const users = user.id
-          ? [...oldUsers.filter((u) => u.id !== user.id)]
-          : [...oldUsers];
-        return [
-          ...users,
-          { ...user, id: user.id ? user.id : shortid.generate() },
-        ];
-      });
-
-      return previousValue;
+  const registerSubUser = useMutation(
+    (user) => {
+      const { name, email, type, oldEmail, admin } = user;
+      registerUser(name, email, null, type, admin, oldEmail);
     },
-    onError: (err, variables, previousValue) => {
-      setStatus('failed');
-      return queryClient.setQueryData('users', previousValue);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries('users');
-      setStatus('success');
-      if (cb) cb();
-    },
-    onSettled: () => queryClient.refetchQueries('users'),
-  });
+    {
+      onMutate: async (user) => {
+        setStatus('pending');
+        await queryClient.cancelQueries('users');
+
+        const previousValue = queryClient.getQueryData('users');
+
+        queryClient.setQueryData('users', (oldUsers) => {
+          const users = user.id
+            ? [...oldUsers.filter((u) => u.id !== user.id)]
+            : [...oldUsers];
+          return [
+            ...users,
+            { ...user, id: user.id ? user.id : shortid.generate() },
+          ];
+        });
+
+        return previousValue;
+      },
+      onError: (err, variables, previousValue) => {
+        setStatus('failed');
+        return queryClient.setQueryData('users', previousValue);
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+        setStatus('success');
+        if (cb) cb();
+      },
+      onSettled: () => queryClient.refetchQueries('users'),
+    }
+  );
 
   return { registerSubUser: registerSubUser.mutate, status };
 }
